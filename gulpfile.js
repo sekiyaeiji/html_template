fdeefe5314acfb0886b0e1024bb4d5623935e9c4
sekiya <sekiya@gnavi.co.jp>
2017-08-24 14:11:00 +0900

    iconなど

--- gulpfile.js
+++ gulpfile.js
@@ -46,9 +46,10 @@ const rename = require('gulp-rename')
 const size = require('gulp-size')
 const postcss = require('gulp-postcss')
 const moment = require('momentjs')
-//const timestump = moment().format('YYYYMMDDhhmmss')
-const timestump = '20161012113446'
-const version = require('./version.json')
+const timestump = moment().format('YYYYMMDDhhmmss')
+//const timestump = '20161012113446'
+const version = require('./gulp/version.json')
+const path = require('./gulp/path.json')
 
 
 /*
@@ -56,50 +57,45 @@ const version = require('./version.json')
  */
 // build:css
 gulp.task('build:css', function () {
-  runSequence('precss', 'renamecss', 'postcss');
-});
+  runSequence('precss', 'renamecss', 'postcss')
+})
 
 // build:js
 gulp.task('build:js', function () {
-  runSequence('babel', 'concat', 'test');
-});
-
-// build:js lib
-gulp.task('concat', function () {
-  runSequence('concat:lib');
-});
+  runSequence('babel', 'test')
+})
 
 // build:html
 gulp.task('build:html', function () {
-  runSequence('ejs');
-});
+  runSequence('ejs')
+})
 
 // build:copy
 gulp.task('build:copy', function () {
-  runSequence('copy');
-});
+  runSequence('copy')
+})
 
 // imagemin
 gulp.task('imagemin', function () {
-  runSequence('imageMin');
-});
+  runSequence('imageMin')
+})
 
 // test
 gulp.task('test', function () {
-  runSequence('eslint');
-});
+  runSequence('eslint')
+})
 
 // build
 gulp.task('build', function () {
   return runSequence(
     'build:css', 'build:js', 'build:html', 'build:copy'
-  );
-});
+  )
+})
 
 // default
 gulp.task('default', function () {
-  runSequence('build');
-});
+  runSequence('build')
+})
 
 
 /*
@@ -109,59 +105,30 @@ gulp.task('default', function () {
 gulp.task('start', function () {
   return runSequence(
     'build', 'watch', 'serve'
-  );
-});
+  )
+})
 
 // local
 gulp.task('local', function () {
-  runSequence('build');
-});
+  runSequence('build')
+})
 
 // dev
 gulp.task('dev', function () {
-  runSequence('build');
-});
-
-
-/*
- * path
- */
-const path = {
-  src: 'src/',
-  dist: 'dist/',
-  tmp: 'tmp/',
-  html_src: 'src/ejs/',
-  css_src: 'src/css/',
-  styleguide_src: 'src/styleguide/',
-  js_src: 'src/js/',
-  img_src: 'src/img/',
-  sprite_src: 'src/sprite/'
-};
-
-
-/*
- * stat path
- */
-const pathStat = {
-  local: './',
-  test_x: 'http://hoge/'
-};
+  runSequence('build')
+})
 
 
 /*
  * js parts
  */
 const js_part = {
-  lib: [
-    path.js_src + 'lib/jquery-3.2.0.min.js',
-    path.js_src + 'lib/lodash.min.js'
-  ],
   common: [
-    path.js_src + 'common/utility.js',
-    path.js_src + 'common/ajax.js',
-    path.js_src + 'common/ui.js'
+    path.dir.js_src + 'common/utility.js',
+    path.dir.js_src + 'common/ajax.js',
+    path.dir.js_src + 'common/ui.js'
   ]
-};
+}
 
 
 /*
@@ -174,47 +141,47 @@ gulp.task('serve', function () {
       port: 8052
     },
     server: {
-      baseDir: path.dist
+      baseDir: path.dir.dist
     }
-  };
-  browserSync(syncOption);
-});
+  }
+  browserSync(syncOption)
+})
 
 
 /*
  * watch
  */
 gulp.task('watch', function () {
-  console.log('---------- watch ----------');
+  console.log('---------- watch ----------')
   return (function(){
-    gulp.watch(path.css_src + '**/*.css', ['build:css']).on('change', browserSync.reload);
-    gulp.watch(path.js_src + '**/*.js', ['build:js']).on('change', browserSync.reload);
-    gulp.watch(path.html_src + '**/*.{ejs,json}', ['build:html']).on('change', browserSync.reload);
-    gulp.watch(path.img_src + '**/*.{png,jpg}', ['build:copy']).on('change', browserSync.reload);
-    gulp.watch('gulpfile.js', ['build']).on('change', browserSync.reload);
-  })();
-});
+    gulp.watch(path.dir.css_src + '**/*.css', ['build:css']).on('change', browserSync.reload)
+    gulp.watch(path.dir.js_src + '**/*.js', ['build:js']).on('change', browserSync.reload)
+    gulp.watch(path.dir.html_src + '**/*.{ejs,json}', ['build:html']).on('change', browserSync.reload)
+    gulp.watch(path.dir.img_src + '**/*.{png,jpg}', ['build:copy']).on('change', browserSync.reload)
+    gulp.watch('gulpfile.js', ['build']).on('change', browserSync.reload)
+  })()
+})
 
 
 /*
  * clean
  */
-const clean = require('del');
+const clean = require('del')
 gulp.task('clean', function () {
-  console.log('---------- clean ----------');
-  clean(path.tmp);
-  clean(path.dist);
-  clean(path.tmp_dev);
-});
+  console.log('---------- clean ----------')
+  clean(path.dir.tmp)
+  clean(path.dir.dist)
+  clean(path.dir.tmp_dev)
+})
 
 
 /*
  * sprite
  */
-const spritesmith = require('gulp.spritesmith');
+const spritesmith = require('gulp.spritesmith')
 gulp.task('sprite', function () {
-  console.log('---------- sprite ----------');
-  const spriteData = gulp.src(path.sprite_src + 'sprite-icon/*.png')
+  console.log('---------- sprite ----------')
+  const spriteData = gulp.src(path.dir.sprite_src + 'sprite-icon/*.png')
   .pipe(spritesmith({
     imgName: 'sprite-icon.png',
     cssName: 'sprite-icon.css',
@@ -226,60 +193,60 @@ gulp.task('sprite', function () {
       return '.icon--' + sprite.name;
     }
   }
-  }));
-  spriteData.img.pipe(gulp.dest(path.img_src));
-  spriteData.css.pipe(gulp.dest(path.css_src + 'common/module/'))
-    .pipe(size({title:'size : sprite'}));
-});
+  }))
+  spriteData.img.pipe(gulp.dest(path.dir.img_src))
+  spriteData.css.pipe(gulp.dest(path.dir.css_src + 'common/module/'))
+    .pipe(size({title:'size : sprite'}))
+})
 
 
 /*
  * imageMin
  */
-const imagemin = require('gulp-imagemin');
+const imagemin = require('gulp-imagemin')
 gulp.task('imageMin', function() {
-  console.log('---------- imageMin ----------');
-  return gulp.src(path.img_src + '**/*')
+  console.log('---------- imageMin ----------')
+  return gulp.src(path.dir.img_src + '**/*')
     .pipe(imagemin({
       progressive: true,
       interlaced: true
     }))
-    .pipe(gulp.dest(path.img_src));
-});
+    .pipe(gulp.dest(path.dir.img_src))
+})
 
 
 /*
  * postcss
  */
 // precss(scss like)
-const precss = require('precss');
+const precss = require('precss')
 gulp.task('precss', function () {
-  console.log('---------- css ----------');
-  return gulp.src(path.css_src + '**/*.css')
+  console.log('---------- css ----------')
+  return gulp.src(path.dir.css_src + '**/*.css')
     .pipe(plumber({
       errorHandler: notify.onError('Error: <%= error.message %>')
     }))
     .pipe(postcss([
         precss()
     ]))
-    .pipe(gulp.dest(path.tmp + 'css/'));
-});
+    .pipe(gulp.dest(path.dir.tmp + 'css/'))
+})
 
 // rename
 gulp.task('renamecss', function () {
-  return gulp.src(path.tmp + 'css/common/import.css')
+  return gulp.src(path.dir.tmp + 'css/common/import.css')
     .pipe(plumber({
       errorHandler: notify.onError('Error: <%= error.message %>')
     }))
     .pipe(rename('common-' + version.css.common + '.css'))
-    .pipe(gulp.dest(path.tmp + 'css/'));
-});
+    .pipe(gulp.dest(path.dir.tmp + 'css/'))
+})
 
 // postcss
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
 gulp.task('postcss', function () {
-  return gulp.src(path.tmp + 'css/*.css')
+  return gulp.src(path.dir.tmp + 'css/*.css')
     .pipe(plumber({
       errorHandler: notify.onError('Error: <%= error.message %>')
     }))
@@ -294,36 +261,36 @@ gulp.task('postcss', function () {
         }
       })
     ]))
-    .pipe(gulp.dest(path.dist + 'css/'))
-    .pipe(size({title:'size : css'}));
-});
+    .pipe(gulp.dest(path.dir.dist + 'css/'))
+    .pipe(size({title:'size : css'}))
+})
 
 
 /*
  * kss styleguide
  */
-const kss = require('gulp-kss');
+const kss = require('gulp-kss')
 gulp.task('styleguide', function () {
-  return gulp.src(path.tmp + 'css/common.css')
+  return gulp.src(path.dir.tmp + 'css/common.css')
     .pipe(kss({
-      overview: path.styleguide_src + 'styleguide.md'
+      overview: path.dir.styleguide_src + 'styleguide.md'
     }))
-    .pipe(gulp.dest(path.dist + 'styleguide/'));
-});
+    .pipe(gulp.dest(path.dir.dist + 'styleguide/'))
+})
 
 
 /*
  * js
  */
 // es2015
-const babel = require('gulp-babel');
-const concat = require('gulp-concat-util');
-const minify = require('gulp-babel-minify');
-const uglify = require('gulp-uglify');
+const babel = require('gulp-babel')
+const concat = require('gulp-concat-util')
+const minify = require('gulp-babel-minify')
+const uglify = require('gulp-uglify')
 
 // babel
 gulp.task('babel', function () {
-  console.log('---------- js ----------');
+  console.log('---------- js ----------')
   return gulp.src(js_part.common)
     .pipe(plumber({
       errorHandler: notify.onError('Error: <%= error.message %>')
@@ -336,9 +303,9 @@ gulp.task('babel', function () {
     ].join('\n')))
     .pipe(concat.footer([
       '',
-      '})(window, window.jQuery);'
+      '})(window, window.jQuery)'
     ].join('\n')))
-    .pipe(gulp.dest(path.tmp + 'js/'))
+    .pipe(gulp.dest(path.dir.tmp + 'js/'))
     .pipe(babel({
       filename: 'common-' + version.js.common + '.js',
       presets: [["es2015", {"loose": true}]],
@@ -347,46 +314,34 @@ gulp.task('babel', function () {
       comments: false
     }))
     .pipe(uglify())
-    .pipe(gulp.dest(path.dist + 'js/'))
-    .pipe(size({title:'size : js common'}));
-});
-
-// concat
-// lib
-gulp.task('concat:lib', function () {
-  return gulp.src(js_part.lib)
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')
-    }))
-    .pipe(concat('lib-' + version.js.lib + '.js'))
-    .pipe(gulp.dest(path.dist + 'js/'))
-    .pipe(size({title:'size : js lib'}));
-});
+    .pipe(gulp.dest(path.dir.dist + 'js/'))
+    .pipe(size({title:'size : js common'}))
+})
 
 
 /*
  * js test
  */
 // eslint
-const eslint = require('gulp-eslint');
+const eslint = require('gulp-eslint')
 gulp.task('eslint', function () {
-  return gulp.src(path.tmp + 'js/common.js')
-    .pipe(eslint('eslint-config-gnavi'));
-});
+  return gulp.src(path.dir.tmp + 'js/common.js')
+    .pipe(eslint('eslint-config-gnavi'))
+})
 
 
 /*
  * html
  */
 // ejs
-const ejs = require('gulp-ejs');
-const minifyejs = require('gulp-minify-ejs');
+const ejs = require('gulp-ejs')
+const minifyejs = require('gulp-minify-ejs')
 gulp.task('ejs', function() {
-  console.log('---------- html ----------');
+  console.log('---------- html ----------')
   gulp.src(
       [
-        path.html_src + 'html/**/*.ejs',
-        '!' + path.html_src + 'html/include/**/*.ejs'
+        path.dir.html_src + 'html/**/*.ejs',
+        '!' + path.dir.html_src + 'html/include/**/*.ejs'
       ]
     )
     .pipe(plumber({
@@ -395,36 +350,36 @@ gulp.task('ejs', function() {
     .pipe(ejs(
       {
         data:{
-          default: require('./' + path.html_src + 'data/common/default.json'),
-          nav: require('./' + path.html_src + 'data/common/nav.json'),
-          sample: require('./' + path.html_src + 'data/module/sample.json'),
-          version: require('./version.json')
+          default: require('./' + path.dir.html_src + 'data/common/default.json'),
+          nav: require('./' + path.dir.html_src + 'data/common/nav.json'),
+          sample: require('./' + path.dir.html_src + 'data/module/sample.json'),
+          version: require('./gulp/version.json')
         },
         timestump: timestump,
-        pathStat: pathStat.local
+        path: path.stat.local
       },
       {ext: '.html'}
     ))
-    .pipe(gulp.dest(path.dist + '/'))
-    .pipe(size({title:'size : html'}));
-});
+    .pipe(gulp.dest(path.dir.dist + '/'))
+    .pipe(size({title:'size : html'}))
+})
 
 
 /*
  * copy
  */
 gulp.task('copy', function () {
-  console.log('---------- copy ----------');
+  console.log('---------- copy ----------')
   return gulp.src(
     [
-      path.img_src + '**/*'
+      path.dir.img_src + '**/*'
     ],
-    {base: path.src}
+    {base: path.dir.src}
   )
   .pipe(plumber({
     errorHandler: notify.onError('Error: <%= error.message %>')
   }))
-  .pipe(gulp.dest(path.dist));
-});
+  .pipe(gulp.dest(path.dir.dist))
+})
 
 
